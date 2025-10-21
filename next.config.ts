63724ce0d826f1d179594d4e9e828dab5e944f99
Alexander Tsekhansky <alexins9@gmail.com>
2025-10-21 20:07:02 -0300

    fix base path

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
+const repo = process.env.REPO || 'virario_website';
+const useRepoSubpath = process.env.USE_REPO_BASEPATH === 'true';
 
 const nextConfig = {
   output: 'export',
+  
   // trailingSlash: true, // optional
-  images: { unoptimized: true } // or use a custom loader if needed
+  basePath: useRepoSubpath ? `/${repo}` : '',        // '' for custom domain
+  assetPrefix: useRepoSubpath ? `/${repo}/` : undefined,
+  images: { unoptimized: true } // static export compatibility
 };
 
 export default nextConfig;
